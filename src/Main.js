2531e2d3a7473d4376830c910a32b2ea3c463ec5
Jacob Miecznikowski <jmieczni@purdue.edu>
2017-07-09 20:55:56 -0400

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <p>
+        Hello, World!
+      </p>
+    )
+  }
+}
+
+export default Main
